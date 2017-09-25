6d7c06e31927188b9174f2b6cba56cb51976ce99
Apurva Kumari <apurvakumari@Apurvas-MacBook-Pro.local>
2017-09-24 20:06:06 -0700

    Initial Commit

---
+++ MapAssignment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MapAssignment
+//
+//  Created by Apurva Kumari on 9/24/17.
+//  Copyright © 2017 Apurva Kumari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
